20a099abc0fcc91f01627c8b27b53ab561056a92
Ravinder Singh Negi <86410071+Ravinder2001@users.noreply.github.com>
2022-06-25 21:14:32 +0530

    Update SignIn.js

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -12,99 +12,151 @@ import {
 import React, {useState} from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Email from 'react-native-vector-icons/Entypo';
+import * as yup from 'yup';
+import {Formik} from 'formik';
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 const SignIn = () => {
   const [secure, setSecure] = useState(false);
+  const [status, setStatus] = useState(false);
+  const schema = yup.object().shape({
+    email: yup
+      .string()
+      .email('Please enter valid email')
+      .required('Email address is required'),
+    password: yup.string().required('Password is required'),
+  });
   return (
-    <View style={styles.root}>
-      <View style={styles.inputBox}>
-        <Email name="email" size={28} style={styles.icon} />
-        <TextInput
-          placeholder="Email Address"
-          style={{...styles.input, marginLeft: 3}}
-        />
-      </View>
-      <View style={styles.inputBox}>
-        <Icon name="lock" size={28} style={styles.icon} />
-        <TextInput
-          placeholder="Password"
-          secureTextEntry={!secure}
-          style={{...styles.input, marginLeft: 10}}
-        />
-        {secure ? (
+    <Formik
+      initialValues={{name: '', email: '', password: ''}}
+      validateOnMount={true}
+      onSubmit={values => alert(JSON.stringify(values))}
+      validationSchema={schema}>
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        touched,
+        errors,
+        isValid,
+      }) => (
+        <View style={styles.root}>
+          <View style={styles.inputBox}>
+            <Email name="email" size={28} style={styles.icon} />
+            <TextInput
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+              placeholder="email"
+              style={{...styles.input, marginLeft: 3}}
+            />
+          </View>
+          {status ? (
+            <View>
+              {errors.email && (
+                <Text style={styles.errors}>{errors.email}</Text>
+              )}
+            </View>
+          ) : null}
+          <View style={styles.inputBox}>
+            <Icon name="lock" size={28} style={styles.icon} />
+            <TextInput
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              value={values.password}
+              placeholder="password"
+              secureTextEntry={!secure}
+              style={{...styles.input, marginLeft: 10}}
+            />
+            {secure ? (
+              <Pressable
+                onPress={() => {
+                  setSecure(false);
+                }}>
+                <Email name="eye" size={28} style={styles.secure} />
+              </Pressable>
+            ) : (
+              <Pressable
+                onPress={() => {
+                  setSecure(true);
+                }}>
+                <Email name="eye-with-line" size={28} style={styles.secure} />
+              </Pressable>
+            )}
+          </View>
+          {status ? (
+            <View>
+              {errors.password && (
+                <Text style={styles.errors}>{errors.password}</Text>
+              )}
+            </View>
+          ) : null}
+          <View>
+            <Text
+              style={{
+                color: '#7ba6e5',
+                width: '80%',
+                marginHorizontal: '10%',
+                marginTop: 10,
+                fontSize: 18,
+              }}>
+              Forgot Password?
+            </Text>
+          </View>
+
           <Pressable
             onPress={() => {
-              setSecure(false);
+              setStatus(true);
+              handleSubmit();
             }}>
-            <Email name="eye" size={28} style={styles.secure} />
+            <View style={styles.btn}>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  color: 'white',
+                  fontSize: 20,
+                  padding: 10,
+                }}>
+                Login
+              </Text>
+            </View>
           </Pressable>
-        ) : (
-          <Pressable
-            onPress={() => {
-              setSecure(true);
-            }}>
-            <Email name="eye-with-line" size={28} style={styles.secure} />
-          </Pressable>
-        )}
-      </View>
-
-      <View>
-        <Text
-          style={{
-            color: '#7ba6e5',
-            width: '80%',
-            marginHorizontal: '10%',
-            marginTop: 10,
-            fontSize: 18,
-          }}>
-          Forgot Password?
-        </Text>
-      </View>
-
-      <Pressable
-        onPress={() => {
-          alert('Create Account');
-        }}>
-        <View style={styles.btn}>
-          <Text
+          <View
             style={{
-              textAlign: 'center',
-              color: 'white',
-              fontSize: 20,
-              padding: 10,
+              flexDirection: 'row',
+              justifyContent: 'space-evenly',
+              marginTop: 15,
             }}>
-            Login
-          </Text>
+            <Image
+              style={{width: 100, height: 100}}
+              source={{uri: 'https://img.icons8.com/bubbles/344/gmail-new.png'}}
+            />
+            <Image
+              style={{width: 100, height: 100}}
+              source={{
+                uri: 'https://img.icons8.com/bubbles/344/facebook-new.png',
+              }}
+            />
+            <Image
+              style={{width: 100, height: 100}}
+              source={{
+                uri: 'https://img.icons8.com/bubbles/344/instagram-new--v2.png',
+              }}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              marginTop: 37,
+            }}>
+            <Text>By creating an account, you agree to Amazon's</Text>
+            <Text style={{color: '#7ba6e5'}}>Term's of use</Text>
+          </View>
         </View>
-      </Pressable>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
-          marginTop: 15,
-        }}>
-        <Image
-          style={{width: 100, height: 100}}
-          source={{uri: 'https://img.icons8.com/bubbles/344/gmail-new.png'}}
-        />
-        <Image
-          style={{width: 100, height: 100}}
-          source={{uri: 'https://img.icons8.com/bubbles/344/facebook-new.png'}}
-        />
-        <Image
-          style={{width: 100, height: 100}}
-          source={{
-            uri: 'https://img.icons8.com/bubbles/344/instagram-new--v2.png',
-          }}
-        />
-      </View>
-      <View
-        style={{flexDirection: 'row', justifyContent: 'center', marginTop: 37}}>
-        <Text>By creating an account, you agree to Amazon's</Text>
-        <Text style={{color: '#7ba6e5'}}>Term's of use</Text>
-      </View>
-    </View>
+      )}
+    </Formik>
   );
 };
 const styles = StyleSheet.create({
@@ -145,6 +197,11 @@ const styles = StyleSheet.create({
   secure: {
     marginLeft: 10,
     marginTop: 10,
+  },
+  errors: {
+    color: 'red',
+    width: '80%',
+    marginHorizontal: '10%',
   },
 });
 export default SignIn;
