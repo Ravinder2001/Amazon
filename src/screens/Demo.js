dc2d5f31f6ce137ca4a3571e18a45e6a92e8f098
Ravinder <ravinder.s.negi@indusnet.co.in>
2022-07-04 18:29:36 +0530

    google signin

--- src/screens/Demo.js
+++ src/screens/Demo.js
@@ -1,27 +1,28 @@
-import {View, Text, Button} from 'react-native';
-import React from 'react';
-import auth from '@react-native-firebase/auth';
-const Demo = () => {
-  async function signInWithPhoneNumber() {
-    const confirmation = await auth().signInWithPhoneNumber("+919756726341");
-    // setConfirm(confirmation);
-    console.log(confirmation)
+import React, {useState} from 'react';
+import {Button, TextInput} from 'react-native';
+// import auth from '@react-native-firebase/auth';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+GoogleSignin.configure({
+  webClientId: '',
+});
+function Demo() {
+  async function onGoogleButtonPress() {
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
   }
-
-  // async function confirmCode() {
-  //   try {
-  //     await confirm.confirm(code);
-  //   } catch (error) {
-  //     console.log('Invalid code.');
-  //   }
-  // }
-
   return (
-    <View>
-      <Text>Demo</Text>
-      <Button onPress={signInWithPhoneNumber} title="click me" />
-    </View>
+    <Button
+      title="Google Sign-In"
+      onPress={() =>
+        onGoogleButtonPress().then(() => console.log('Signed in with Google!'))
+      }
+    />
   );
-};
-
+}
 export default Demo;
